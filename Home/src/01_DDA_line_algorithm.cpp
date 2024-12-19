458ea3fd39f9e26861bc1c787044535ba687e219
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2024-12-19 22:13:48 +0545

    create SUDOKU with DDA line algorithm

--- Home/src/01_DDA_line_algorithm.cpp
+++ Home/src/01_DDA_line_algorithm.cpp
@@ -4,7 +4,7 @@
 #include <conio.h>
 using namespace std;
 
-void drawLine(int x1, int y1, int x2, int y2){
+void drawLine(int x1, int y1, int x2, int y2, int COLOR){
     float dx = x2 - x1;
     float dy = y2 - y1;
 
@@ -17,7 +17,7 @@ void drawLine(int x1, int y1, int x2, int y2){
     float y = y1;
 
     for(int i = 0 ; i <= steps; i++){
-        putpixel(round(x), round(y), WHITE);
+        putpixel(round(x), round(y), COLOR);
         x += xIncrement;
         y += yIncrement;
     }
@@ -27,8 +27,27 @@ int main () {
     int gd = DETECT, gm;
     initgraph(&gd, &gm, "");
 
-    int x1 = 100, y1 = 100, x2 = 400, y2 = 300;
-    drawLine(x1, y1, x2, y2);
+
+    // SUDOKU BOX using DDA 
+    for(int i = 40; i <= 400; i += 40){
+        if(i == 40 || i == 160 || i == 280 || i == 400){
+            drawLine(40, i, 400, i, WHITE);
+        }
+        else 
+        {
+            drawLine(40, i, 400, i, RED);
+        }
+        
+    }
+    for(int i = 40; i <= 400; i += 40){
+        if(i == 40 || i == 160 || i == 280 || i == 400){
+            drawLine(i, 40, i, 400, WHITE);
+        }
+        else {
+            drawLine(i, 40, i, 400, RED);
+        }
+        
+    }
 
     getch();
     closegraph();
